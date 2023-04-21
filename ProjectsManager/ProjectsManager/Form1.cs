1c341589a5f286df9033cade3919469c0bb99e5c
Mário Lastovica <mario.lastovica228@gmail.com>
2023-04-21 20:58:28 +0200

    Create project and connect to mysql

---
+++ ProjectsManager/ProjectsManager/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjectsManager
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        string connString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connString = "SERVER=usa.vybrat.eu;PORT=3306;DATABASE=c71_database;UID=c71_admin;PASSWORD=Qw6hVp3T!";
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = connString;
+                conn.Open();
+                MessageBox.Show("Connection successfull");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error when connecting to database");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
